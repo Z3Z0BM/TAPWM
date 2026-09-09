288a53a3466ae1bf4bae58ec866ddd69a26cf8df
ENZO BERNARDO MELLOTTO <0031432612043@lti.fatecso.edu.br>
2026-09-09 14:11:01 -0300

    Atualização dos códigos nos arquivos da Atividade 20

---
+++ 2º Semestre/3º Bimestre/Atividade 20/app.js
@@ -0,0 +1,22 @@
+let app = require('./app/config/server');
+
+let rotaHome = require('./app/routes/home');
+rotaHome(app);
+
+let rotaContato = require('./app/routes/contato');
+rotaContato(app);
+
+let rotaInscricao = require('./app/routes/insricao');
+rotaInscricao(app);
+
+let rotaJogos = require('./app/routes/jogos');
+rotaJogos(app);
+
+let rotaSobre = require('./app/routes/sobre');
+rotaSobre(app);
+
+
+app.listen(3000, function()
+{
+    console.log("Servidor iniciado!");
+});
